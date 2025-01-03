d7a933daa542cb4b92d5b98a1a3e6406de8ac308
GopiWebDev <gopisv.webdev@gmail.com>
2025-01-03 19:32:29 +0530

    updated build commands

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js',
+    filename: 'bundle.js',
     publicPath: '/',
   },
   module: {
