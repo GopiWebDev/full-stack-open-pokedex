45a495a0fb9bcf41a16e151adbc4351bc5e48f05
GopiWebDev <gopisv.webdev@gmail.com>
2025-01-03 18:46:41 +0530

    fixed all linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,43 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+  env: {
+    'browser': true, // Browser environment
+    'es6': true, // Enable ES6 features
+    'node': true,
+    'jest/globals': true, // Jest globals
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
+  extends: [
+    'eslint:recommended', // ESLint recommended rules
+    'plugin:react/recommended', // React plugin recommended rules
   ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // Enable JSX syntax
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018, // Support ES2018 syntax
+    sourceType: 'module', // Use ES modules
   },
-  "plugins": [
-    "react", "jest"
+  plugins: [
+    'react', // React plugin
+    'jest', // Jest plugin
   ],
-  "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
-  }
-}
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect React version
+    },
+  },
+  rules: {
+    'indent': ['error', 2], // Enforce 2-space indentation
+    'linebreak-style': ['error', 'unix'], // Enforce Unix-style linebreaks
+    'quotes': ['error', 'single'], // Enforce single quotes
+    'semi': ['error', 'always'], // Require semicolons
+    'eqeqeq': 'error', // Enforce strict equality
+    'no-trailing-spaces': 'error', // Disallow trailing spaces
+    'object-curly-spacing': ['error', 'always'], // Require spacing inside curly braces
+    'arrow-spacing': ['error', { before: true, after: true }], // Enforce arrow function spacing
+    'no-console': 'error', // Disallow console statements
+    'react/prop-types': 0, // Disable React prop-types rule
+  },
+  ignorePatterns: [
+    'dist/**', // Ignore the dist folder
+  ],
+};
