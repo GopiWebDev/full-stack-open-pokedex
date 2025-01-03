724f542ff2a88c8921b38b1234bc94946bace6be
GopiWebDev <gopisv.webdev@gmail.com>
2025-01-03 19:40:10 +0530

    updated webpack path

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
+  context: path.resolve(__dirname),
   entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
